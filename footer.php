d6b5d989e1b859ad97d8f480108f66d5a196c061
Anderson Ryan <santoryan1408@gmail.com>
2022-02-09 20:27:27 -0200

    Netflix Prototype

---
+++ footer.php
@@ -0,0 +1,22 @@
+<footer>
+    <p>Questions? Call 0800-761-4632</p>
+
+    <ul>
+        <li>FAQ</li>
+        <li>Ways to watch</li>
+        <li>Terms of Use</li>
+        <li>Contact Us</li>
+        <li>Privacy</li>
+        <li>Cookie Preferences</li>
+        <li>Legal Notices</li>
+    </ul>
+
+    <select name="" class="select-language">
+                    <option value="">Portuguese</option>
+                    <option value="">English</option>
+    </select>
+
+    <p>Netflix Brazil</p>
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?php bloginfo(); ?></title>
+</head>
+<body>
+    <header>
+        <div>
+            <img src="assets/netflix-logo.jpg" alt="netflix-logo">
+
+            <nav>
+                <select name="" class="select-language">
+                    <option value="">Portuguese</option>
+                    <option value="">English</option>
+                </select>
+                <button>Sign In</button>
+            </nav>
+        </div>
+
+        <h1>
+            Unlimited moves, TV</br> shows, and more.
+        </h1>
+        <h2>Watch anywhere. Cancel anytime.</h2>
+        <p>Ready to watch? Enter your email to create or restart your membership.</p>
+
+        <input type="text" placeholder="Email address">
+        <button class="cta">Get Started ></button>
+    </header>
+   
