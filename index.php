2abfb25d97d80f91c534ea3578c47694edc38c99
Anderson Ryan <santoryan1408@gmail.com>
2022-02-09 20:27:27 -0200

    Netflix Prototype

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+    get_header();
+?>
+
+<section>
+    <h1>Frequently Asked Questions</h1>
+
+    <ul>
+        <li>What is Netflix</li>
+        <li>How much does Netflix cost?</li>
+        <li>Where can I watch?</li>
+        <li>How do I cancel?</li>
+        <li>What can I watch in Netflix?</li>
+        <li>Is Netflix good for kids?</li>
+    </ul>
+
+    <div>
+        <p>Ready to watch? Enter your email to create or restart your membership.</p>
+        <input type="text"  placeholder="Email address">
+        <button class="cta">Get Started ></button>
+    </div>
+</section>
+
+<?php
+    get_footer();
+?>
